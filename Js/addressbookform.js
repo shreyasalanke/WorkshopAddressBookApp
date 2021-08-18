74593707d5d9f3eb9e2748c780f35cd2fcb1eb10
shreyasalanke <shreyasalanke97@gmail.com>
2021-08-18 21:34:53 +0530

    ADDED: Program to save on clicking submit button and save to local

--- Js/addressbookform.js
+++ Js/addressbookform.js
@@ -1,55 +1,66 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-    const fullname = document.querySelector('#fullname');
-    const textError = document.querySelector('.text-error');
-    fullname.addEventListener('input', function () {
-        if (fullname.value.length == 0) {
-            textError.textContent = "";
-            return
-        }
-        try {
-            (new AddressBookContact()).fullName = fullname.value;
-            textError.textContent = "";
-        } catch (e) {
-            textError.textContent = e;
-        }
-    });
 
-    const phoneNo = document.querySelector('#tel');
-    const phoneError = document.querySelector('.mobno-error');
-    phoneNo.addEventListener('input', function () {
-        if (phoneNo.value.length == 0) {
-            phoneError.textContent = "";
-            return
+    const name=document.querySelector('#name');
+    const nameError=document.querySelector('.name-error');
+    name.addEventListener('input',function(){
+        let names=document.querySelector('#name').value.split(" ");
+        if(names[0].length==0){
+            nameError.textContent="";
+            return;
         }
-        try {
-            (new AddressBookContact()).phoneNumber = phoneNo.value;
-            phoneError.textContent = "";
-        } catch (e) {
-            phoneError.textContent = e;
+        if(names.length==2){
+            let nameRegex=RegExp('^[A-Z][a-z]{2,}$');
+            if(!nameRegex.test(names[0])) nameError.textContent="First name is Incorrect";
+            if(!nameRegex.test(names[1])) nameError.textContent="Last name is Incorrect";
+            if(nameRegex.test(names[0])&& nameRegex.test(names[1])) nameError.textContent="";
         }
-    });
+        else{
+            let nameRegex=RegExp('^[A-Z][a-z]{2,}$');
+            if(!nameRegex.test(names[0])) nameError.textContent="First Name is Invalid";
+            else nameError.textContent="";
+        }
+    })
 
-    const address = document.querySelector('#address');
-    const addressError = document.querySelector('.address-error');
-    address.addEventListener('input', function () {
-        if (address.value.length == 0) {
-            addressError.textContent = "";
-            return
-        }
-        try {
-            (new AddressBookContact()).address = address.value;
-            addressError.textContent = "";
-        } catch (e) {
-            addressError.textContent = e;
-        }
-    });
-});
+     const addressElement=document.querySelector('#address');
+     const addressError=document.querySelector('.address-error');
+     addressElement.addEventListener('input',function(){
+         let address=document.querySelector('#address').value;
+         let words=address.split(" ");
+         if(words.length>1){
+             let addressRegex=RegExp('^[A-Za-z,.0-9]{3,}$');
+             for(word of words){
+                 if(!addressRegex.test(word)) addressError.textContent="Please enter atleast 3 letters";
+                 else addressError.textContent="";
+             }
+         }
+         else addressError.textContent="Please enter multiple words";
+     })
 
-const save = (event) => {
-    event.preventDefault();
-    alert("Details submitted successfully");
+    const phoneElement=document.querySelector('#mobile');
+    const phoneError=document.querySelector('.mobile-error');
+    phoneElement.addEventListener('input',function(){
+        let phone=document.querySelector('#mobile').value;
+        let phoneRegex1=RegExp('^[1-9][0-9]{9}$');
+        let phoneRegex2=RegExp('^[0-9]{2}[1-9][0-9]{9}$');
+        let phoneRegex3=RegExp('^[+][0-9]{2}[1-9][0-9]{9}$');
+        if(phoneRegex1.test(phone)||phoneRegex2.test(phone)||phoneRegex3.test(phone)) phoneError.textContent="";
+        else phoneError.textContent="Phone number is Incorrect";
+    })
+
+  });
+
+  const saveForm=(event)=>{
+      createAddressBookData();
   }
-  
-  const resetForm = () => {
-    
+
+  const createAddressBookData=()=>{
+      const addressBook={
+          "fullName":document.querySelector('#name').value,
+          "mobileNumber":document.querySelector('#mobile').value,
+          "address":document.querySelector('#address').value,
+          "city":document.querySelector('#city').value,
+          "state":document.querySelector('#state').value,
+          "zip":document.querySelector('#zip').value
+      }
+      alert(JSON.stringify(addressBook));
   }
