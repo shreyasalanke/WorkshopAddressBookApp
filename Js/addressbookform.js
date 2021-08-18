f43ec7f457a5ee4a485483e71e5ed4294d5d6567
shreyasalanke <shreyasalanke97@gmail.com>
2021-08-18 21:47:58 +0530

    ADDED: Program to populate contact class and capture user entry

--- Js/addressbookform.js
+++ Js/addressbookform.js
@@ -8,46 +8,38 @@ window.addEventListener('DOMContentLoaded', (event) => {
             nameError.textContent="";
             return;
         }
-        if(names.length==2){
-            let nameRegex=RegExp('^[A-Z][a-z]{2,}$');
-            if(!nameRegex.test(names[0])) nameError.textContent="First name is Incorrect";
-            if(!nameRegex.test(names[1])) nameError.textContent="Last name is Incorrect";
-            if(nameRegex.test(names[0])&& nameRegex.test(names[1])) nameError.textContent="";
-        }
-        else{
-            let nameRegex=RegExp('^[A-Z][a-z]{2,}$');
-            if(!nameRegex.test(names[0])) nameError.textContent="First Name is Invalid";
-            else nameError.textContent="";
+        try{
+            (new AddressBook()).fullName=name.value;
+            nameError.textContent="";
+        }catch(e){
+           nameError.textContent=e;
         }
     })
 
      const addressElement=document.querySelector('#address');
      const addressError=document.querySelector('.address-error');
      addressElement.addEventListener('input',function(){
-         let address=document.querySelector('#address').value;
-         let words=address.split(" ");
-         if(words.length>1){
-             let addressRegex=RegExp('^[A-Za-z,.0-9]{3,}$');
-             for(word of words){
-                 if(!addressRegex.test(word)) addressError.textContent="Please enter atleast 3 letters";
-                 else addressError.textContent="";
-             }
+         try{
+             (new AddressBook()).address=addressElement.value;
+             addressError.textContent="";
+         }catch(e){
+             addressError.textContent=e;
          }
-         else addressError.textContent="Please enter multiple words";
      })
 
     const phoneElement=document.querySelector('#mobile');
     const phoneError=document.querySelector('.mobile-error');
     phoneElement.addEventListener('input',function(){
-        let phone=document.querySelector('#mobile').value;
-        let phoneRegex1=RegExp('^[1-9][0-9]{9}$');
-        let phoneRegex2=RegExp('^[0-9]{2}[1-9][0-9]{9}$');
-        let phoneRegex3=RegExp('^[+][0-9]{2}[1-9][0-9]{9}$');
-        if(phoneRegex1.test(phone)||phoneRegex2.test(phone)||phoneRegex3.test(phone)) phoneError.textContent="";
-        else phoneError.textContent="Phone number is Incorrect";
+        try{
+            (new AddressBook()).mobileNumber=phoneElement.value;
+            phoneError.textContent="";
+        }catch(e){
+            phoneError.textContent=e;
+        }
     })
 
   });
+
 
   const saveForm=(event)=>{
       createAddressBookData();
--- Js/contact.js
+++ Js/contact.js
@@ -1,69 +1,82 @@
-class AddressBookContact {
+class AddressBook{
 
-    //Getters & Setters
+    get id() {
+        return this._id;
+    }
+    set id(value) {
+        this._id = value;
+    }
+
+
     get fullName() {
         return this._fullName;
     }
-    set fullName(fullName) {
-        let fullNamePattern = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
-        if (fullNamePattern.test(fullName)) {
-            this._fullName = fullName;
+    set fullName(name) {
+        let names=name.split(" ");
+        
+        if(names.length==2){
+            let nameRegex=RegExp('^[A-Z][a-z]{2,}$');
+            if(!nameRegex.test(names[0])) throw "First name is Incorrect";
+            if(!nameRegex.test(names[1])) throw "Last name is Incorrect";
+            if(nameRegex.test(names[0])&& nameRegex.test(names[1])) {
+                this._fullName = name;
+            }
+            else throw "Full name is Incorrect";
         }
-        else
-            throw 'Invalid FullName !';
+        else{
+            let nameRegex=RegExp('^[A-Z][a-z]{2,}$');
+            if(!nameRegex.test(names[0])) throw "First Name is Invalid";
+            else this._fullName = value;
+        }
+        
     }
     get address() {
         return this._address;
     }
     set address(address) {
-        let words = address.split(" ");
-        let addressPattern = RegExp('([A-Z a-z 0-9]{3,})+');
-        for (const word of words) {
-            if (!addressPattern.test(word))
-                throw 'Invalid Address !';
-        }
-        this._address = address;
+         let words=address.split(" ");
+         if(words.length>1){
+             let addressRegex=RegExp('^[A-Za-z,.0-9]{3,}$');
+             for(let word of words){
+                 if(!addressRegex.test(word)) throw "Please enter atleast 3 letters";
+                 else this._address = address;;
+             }
+         }
+         else throw "Please enter multiple words";
+    }
+    get mobileNumber() {
+        return this._mobileNumber;
+    }
+    set mobileNumber(phone) {
+
+        let phoneRegex1=RegExp('^[1-9][0-9]{9}$');
+        let phoneRegex2=RegExp('^[0-9]{2}[1-9][0-9]{9}$');
+        let phoneRegex3=RegExp('^[+][0-9]{2}[1-9][0-9]{9}$');
+        if(phoneRegex1.test(phone)||phoneRegex2.test(phone)||phoneRegex3.test(phone)) this._mobileNumber = phone;
+        else throw "Phone number is Incorrect";
+        
     }
     get city() {
         return this._city;
     }
-    set city(city) {
-        this._city = city;
+    set city(value) {
+        this._city = value;
     }
     get state() {
         return this._state;
     }
-    set state(state) {
-        this._state = state;
+    set state(value) {
+        this._state = value;
     }
-    get zipcode() {
-        return this._zipcode;
+    get zip() {
+        return this._zip;
     }
-    set zipcode(zipcode) {
-        let zipPattern = RegExp('^[1-9][0-9]{6}$');
-        if(zipPattern.test(zip)){
-            this._zip = zip;  
-        }
-        else
-            throw 'Invalid ZipCode!';
-
-    }
-    get phoneNumber() {
-        return this._phoneNumber;
-    }
-    set phoneNumber(phoneNumber) {
-        let phPattern = RegExp('^[0-9]{2}[1-9][0-9]{9}$');
-        if (phPattern.test(phoneNumber)) {
-            this._phoneNumber = phoneNumber;
-        }
-        else
-            throw 'Invalid Phone Number!';
+    set zip(value) {
+        this._zip = value;
     }
 
-    //Methods
-    toString() {
-        return '[ FullName : ' + this.fullName + ' Address : '
-            + this.address + ' City : ' + this.city + ' State : ' + this.state + ' Zip : ' + this.zip +
-            ' Phone Number : ' + this.phoneNumber + ' ]';
+    toString(){
+        return "fullName "+this._fullName+"mobileNumber "+this._mobileNumber+"address "+this._address+" city "+
+        this.city+"state "+this.state+"zip "+this.zip;
     }
 }
